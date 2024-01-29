48aa809c8589a7ad64dbc33715b9bcb266e5ad8d
NgigiN <ngigi.nyongo@gmail.com>
2024-01-29 15:51:02 +0300

    Learning how to use Webpack with a config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function () {
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<button>Click here to get started</button>');
+	$('body').append('<p id="count"></p>');
+	$('body').append('<p>Copyright - Holberton School');
+	let count = 0;
+
+	function updateCounter() {
+		count++;
+		$('#count').html(`${count} clicks on the button`);
+	}
+
+	$('#startButton').on('click', _.debounce(updateCounter, 1000));
+});
