7918caad7c16de00323bc80999ef51b1ffb2443d
NgigiN <ngigi.nyongo@gmail.com>
2024-02-07 15:40:15 +0300

    Basic components

---
+++ 0x02-react-props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
+import "../Footer/Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+
+function Footer() {
+  return (
+    <>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
+    </>
+  );
+}
+
+export default Footer;
