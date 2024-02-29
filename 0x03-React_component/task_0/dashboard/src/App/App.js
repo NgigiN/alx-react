7bdd8a71696a41e2d56bc9e6b0ede507173854d1
NgigiN <ngigi.nyongo@gmail.com>
2024-02-29 21:36:18 +0300

    Commence with the class components

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notification from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
+
+
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+];
+
+const listNotifications = [
+  { id: 1, value: "New course available", type: "default" },
+  { id: 2, value: "New resume available", type: "urgent" },
+  { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Notification listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: true
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+export default App;
